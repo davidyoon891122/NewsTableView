c5c2a8ce9ed5c014bfa1c6a0811b4b249ee6e7af
davidyoon <qqpo12@naver.com>
2021-07-14 23:17:06 +0900

    add news json data from site and set news title on table cell

--- NewsTableView/ViewController.swift
+++ NewsTableView/ViewController.swift
@@ -11,9 +11,15 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     @IBOutlet var tableView: UITableView!
     
+    var newsData: Array<Dictionary<String, Any>>?
+    
     // 데이터가 몇개인지
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 100
+        if let news = newsData{
+            return news.count
+        } else {
+            return 0
+        }
     }
     
     // 데이터가 무엇인지
@@ -21,8 +27,18 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         //let cell = UITableViewCell.init(style: .default, reuseIdentifier: "myCell")
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath) as! MyCell //다시 재사용할 수 있는 셀은 재정의 해서 테이블 뷰에 넣는다.
+        let idx = indexPath.row
+        if let news = newsData {
+            let row = news[idx]
+            if let value = row as? Dictionary<String,Any> {
+                if let title = value["title"] as? String{
+                    cell.lblMyCell.text = title
+                }
+                
+            }
+        }
         
-        cell.lblMyCell.text = String(indexPath.row)
+//        cell.lblMyCell.text = String(indexPath.row)
         // as? 맞으면 형변환 as! 강제로  형변환 MyCell : UITableViewCell
         //cell.textLabel?.text = String(indexPath.row)
         return cell
@@ -34,6 +50,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         // Do any additional setup after loading the view.
         tableView.delegate = self
         tableView.dataSource = self // 클래스안에 func 들
+        getNews()
         
     }
 
@@ -43,6 +60,29 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     // 행이 선택될때 액션
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("Clicked !!  \(indexPath.row)")
+    }
+    
+    
+    
+    func getNews() {
+        let task = URLSession.shared.dataTask(with: URL(string: "https://newsapi.org/v2/top-headlines?country=kr&apiKey=05409300ee7a4b688b07cd7985890cd9")!) { data, response, error in
+            if let dataJson = data {
+                //JSON으로 데이터 parsing
+                do {
+                    let json = try JSONSerialization.jsonObject(with: dataJson, options: []) as! Dictionary<String, Any>
+                    let articles = json["articles"] as! Array<Dictionary<String,Any>>
+                    self.newsData = articles
+                    //네트워크 통신은 메인에서 화면 그리게 올려주어야 한다
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                    }
+                    
+                }
+                catch {}
+            }
+        }
+        
+        task.resume()
     }
 }
 
